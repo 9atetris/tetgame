21283b31566c895a414d3b79cc8d172288a7a5fc
9atetris <tetrisotoko777@gmail.com>
2024-11-15 13:41:43 +0700

    modify button and text position

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
+import './index.css';
+import './App.css';
 import { useEffect, useState } from "react";
 import { ArgentTMA, SessionAccountInterface } from "@argent/tma-wallet";
 import Game from './components/Game';
 
 const argentTMA = ArgentTMA.init({
-  environment: "sepolia", // "sepolia" | "mainnet" (not supported yet)
-  appName: "tetgame", // Your Telegram app name
-  appTelegramUrl: "https://t.me/tetgame_bot/tetgame", // Your Telegram app URL
+  environment: "sepolia",
+  appName: "tetgame",
+  appTelegramUrl: "https://t.me/tetgame_bot/tetgame",
   sessionParams: {
     allowedMethods: [
-      // List of contracts/methods allowed to be called by the session key
       {
         contract:
           "0x5f2bf091c03af0f14973d16b556033e0c0e31d5c2ff6199af68d787854c011c",
         selector: "claimPoints",
       }
     ],
-    validityDays: 90 // session validity (in days) - default: 90
+    validityDays: 90
   },
 });
 
@@ -24,36 +25,26 @@ function App() {
   const [isConnected, setIsConnected] = useState<boolean>(false);
 
   useEffect(() => {
-    // Call connect() as soon as the app is loaded
     argentTMA
       .connect()
       .then((res) => {
         if (!res) {
-          // Not connected
           setIsConnected(false);
           setAccount(undefined);
           return;
         }
 
-        // Connected
         const { account, callbackData } = res;
 
-        // Check session status
         const sessionStatus = account.getSessionStatus();
         if (sessionStatus !== "VALID") {
-          // Session has expired or scope (allowed methods) has changed
-          // A new connection request should be triggered
-          // The account object is still available to get access to user's address
-          // but transactions can't be executed
           setAccount(account);
           setIsConnected(false);
           return;
         }
 
-        // Session is valid
         setAccount(account);
         setIsConnected(true);
-        // Custom data passed to the requestConnection() method is available here
         console.log("callback data:", callbackData);
       })
       .catch((err) => {
@@ -64,11 +55,9 @@ function App() {
   }, []);
 
   const handleConnectButton = async () => {
-    // If not connected, trigger a connection request
     await argentTMA.requestConnection("custom_callback_data");
   };
 
-  // Useful for debugging
   const handleClearSessionButton = async () => {
     await argentTMA.clearSession();
     setAccount(undefined);
@@ -79,40 +68,21 @@ function App() {
 
   return (
     <div className="h-auto">
-      {/* Navbar */}
-      <div className="bg-gray-700 p-4 flex ">
-        {!isConnected && (
-          <button
-            className="py-2 px-4 rounded-lg bg-slate-300 text-gray-700"
-            onClick={handleConnectButton}
-          >
-            Connect
-          </button>
-        )}
-
-        {account && (
-          <div className="flex justify-between items-center w-full">
-            <p className="text-green-500 text-[12px]">
-              Account address: <code>{account.address.slice(0, 12)} ...</code>
-            </p>
-            <button
-              className="text-sm p-2 bg-white rounded-lg bg-tomato-300"
-              onClick={handleClearSessionButton}
-            >
-              Clear Session
-            </button>
-          </div>
-        )}
-      </div>
       {/* Content */}
       <div className="grid grid-cols-1">
         {isConnected ? (
           <Game account={account} />
         ) : (
-          <div className="w-full h-screen flex items-center justify-center">
-            <div className="text-center">
-              <p>Please connect your wallet to start the game.</p>
-            </div>
+          <div className="w-full h-screen flex flex-col items-center justify-center">
+            <button
+              className="py-2 px-4 rounded-lg bg-slate-300 text-gray-700"
+              onClick={handleConnectButton}
+            >
+              Connect
+            </button>
+            <p className="mt-4 text-center">
+              Please connect your wallet to start the game.
+            </p>
           </div>
         )}
       </div>
